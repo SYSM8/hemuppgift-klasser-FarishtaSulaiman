5096124bd67a8f4cc71f54a99ed69b9197e30d0a
HP <farishtasulaiman@gmail.com>
2024-09-23 09:50:03 +0200

    klar

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             //Instansiera bankAccount klassen och testa den
+
+            // Nytt bankkonto
+            BankAccount account = new BankAccount("0987654", "Farishta Sulaiman", 1000);
+
+            // Visar det aktuella saldot på kontot
+             account.DisplayBalance();
+
+            // Gör en insättning 
+            account.Deposit(1000);
+
+            //Gör ett uttag
+            account.Withdraw(300);
+
+            //ta ut mer pengar än vad som finns på kontot
+            account.Withdraw(2000);
+
+            account.DisplayBalance();
+
+
         }
     }
 }
